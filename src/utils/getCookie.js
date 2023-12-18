e537e2253fbcede0faa50d38da151e702dff5977
Dmitry <teoahoo@gmail.com>
2023-12-18 16:41:10 +0800

    fix: trash

--- src/utils/getCookie.js
+++
@@ -1,7 +0,0 @@
-const getCookie = function(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-export default getCookie;
