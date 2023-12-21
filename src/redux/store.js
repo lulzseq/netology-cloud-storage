e5d282900aea01890a0f2be9c305db0230f6b9c4
Dmitry <teoahoo@gmail.com>
2023-12-21 15:23:24 +0800

    feat: add admin reducer to configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./slices/authSlice";
 import fileReducer from "./slices/fileSlice";
+import adminReducer from "./slices/adminSlice";
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     file: fileReducer,
+    admin: adminReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
